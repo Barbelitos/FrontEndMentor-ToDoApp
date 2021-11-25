0241c7100f0b26225ce8cc5eaee4d17b0c6995a4
David Fonseca <davidfonseca@barbelitos-mbp.lan>
2021-11-25 14:26:10 +0200

    Todo basic functionalities and filters finished.

--- scripts/main.js
+++ scripts/main.js
@@ -1,109 +1,103 @@
-"use strict";
+'use strict';
 
 // Selectors
+const createTodo = document.getElementById('create-todo');
+const todoList = document.querySelector('.todo-list');
+const todoItem = document.querySelectorAll('.todo-item');
+const todoText = document.querySelectorAll('.todo-text');
+const removeButton = document.querySelectorAll('.remove-todo');
+const counter = document.querySelector('.items-left');
+const clearDone = document.querySelector('.clear-completed');
 
-const createTodo = document.getElementById("create-todo");
-const todoList = document.querySelector(".todo-list");
-const todoItem = document.querySelectorAll(".todo-item");
-const todoText = document.querySelectorAll(".todo-text");
-const removeButton = document.querySelectorAll(".remove-todo");
-const counter = document.querySelector(".items-left");
-const clearDone = document.querySelector(".clear-completed");
+const showAll = document.getElementById('all');
+const showActive = document.getElementById('active');
+const showCompleted = document.getElementById('completed');
 
-const showAll = document.getElementById("all");
-const showActive = document.getElementById("active");
-const showCompleted = document.getElementById("completed");
-
-// Lists
+// Lists Arrays
 let todos = [];
-let completed = [];
 let active = [];
+let completed = [];
 
 // Functions
-
-function addTodo(todo) {
-  let li = document.createElement("li");
+const addTodo = function (todo) {
+  const li = document.createElement('li');
 
   li.innerHTML = `
     <label class="task"><input type="checkbox" name="checkButton" class="todo-li" /><span class="checkmark"></span></label><p class="todo-text">${todo}</p><img class="remove-todo" src="./images/icon-cross.svg" alt="delete todo" />
     `;
-  li.classList.add("item");
-  li.classList.add("todo-item");
+
+  li.classList.add('item');
+  li.classList.add('todo-item');
+
+  if (showCompleted.checked) {
+    li.style.display = 'none';
+  }
+
   todos.push(li);
+  active.push(li);
   todoList.appendChild(li);
-}
+};
 
-function updateCounter() {
+const updateCounter = () => {
   const count = todoList.childElementCount;
-
   counter.innerHTML = count;
-}
+};
 
-updateCounter();
+const renderCompleted = () => {
+  todos.map(todo => {
+    todo.classList.contains('checked-item')
+      ? (todo.style.display = 'flex')
+      : (todo.style.display = 'none');
+  });
+};
+
+const renderActive = () => {
+  active.map(todo => (todo.style.display = 'flex'));
+  completed.map(todo => (todo.style.display = 'none'));
+};
+
+const renderAll = () => todos.map(todo => (todo.style.display = 'flex'));
 
 //Event Handlers
-createTodo.addEventListener("keydown", (e) => {
-  if (e.key === "Enter" && createTodo.value != "") {
+createTodo.addEventListener('keydown', e => {
+  if (!createTodo.value) return;
+
+  if (e.key === 'Enter') {
     addTodo(createTodo.value);
-    createTodo.value = "";
     updateCounter();
+    createTodo.value = '';
   }
 });
 
-todoList.addEventListener("click", (e) => {
-  if (e.target.name === "checkButton") {
-    e.target.parentElement.nextElementSibling.classList.toggle("checked-text");
-    e.target.parentElement.parentElement.classList.toggle("checked-item");
+todoList.addEventListener('click', e => {
+  if (e.target.name === 'checkButton') {
+    e.target.parentElement.nextElementSibling.classList.toggle('checked-text');
+    e.target.parentElement.parentElement.classList.toggle('checked-item');
     if (
-      e.target.parentElement.parentElement.classList.contains("checked-item")
+      e.target.parentElement.parentElement.classList.contains('checked-item')
     ) {
       completed.push(e.target.parentElement.parentElement);
     }
   }
 
-  if (e.target.classList[0] === "remove-todo") {
+  if (e.target.classList[0] === 'remove-todo') {
     e.target.parentElement.remove();
     updateCounter();
   }
 });
 
-clearDone.addEventListener("click", () => {
-  for (let i = 0; i < todos.length; i++) {
-    if (todos[i].classList.contains("checked-item")) {
-      if (completed.includes(todos[i])) {
-        todos[i].remove();
-      }
-    }
-  }
-  todos = todos.filter((el) => {
-    return !completed.includes(el);
-  });
+clearDone.addEventListener('click', () => {
+  todos.map(todo =>
+    todo.classList.contains('checked-item') && completed.includes(todo)
+      ? todo.remove()
+      : ''
+  );
+
+  todos = todos.filter(el => !completed.includes(el));
   completed = [];
   updateCounter();
 });
 
-showAll.addEventListener("click", () => {
-  for (let i = 0; i < todos.length; i++) {
-    todos[i].style.display = "flex";
-  }
-});
-
-showActive.addEventListener("click", () => {
-  for (let i = 0; i < todos.length; i++) {
-    if (todos[i].classList.contains("checked-item")) {
-      todos[i].style.display = "none";
-    } else {
-      todos[i].style.display = "flex";
-    }
-  }
-});
-
-showCompleted.addEventListener("click", () => {
-  for (let i = 0; i < todos.length; i++) {
-    if (!todos[i].classList.contains("checked-item")) {
-      todos[i].style.display = "none";
-    } else {
-      todos[i].style.display = "flex";
-    }
-  }
-});
+showCompleted.addEventListener('click', renderCompleted);
+showActive.addEventListener('click', renderActive);
+showAll.addEventListener('click', renderAll);
